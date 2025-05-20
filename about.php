e941ad051170e06a5f875de58ceb5719c8bd8870
Sandrine Barrucand <sandrine.barrucand@gmail.com>
2025-05-20 13:42:19 +0200

    Update about.php

--- about.php
+++ about.php
@@ -7,7 +7,7 @@ include('header.php');
 
     <main>
         <h2>À propos</h2>
-        <p>Ceci est la page à propos de mon merveilleux site web.</p>
+        <p>Ceci est la page à propos de mon site web.</p>
     </main>
 
 <?php include('footer.php'); ?>
